781191579c1bbc5cc1f33b1b333832db6466cbca
Dhuy2708 <vodinhhuy2708@gmail.com>
2025-02-10 00:55:59 +0700

    feat: blob service

---
+++ LiftNet.Api/Extensions/ExtenalServiceExtension.cs
@@ -0,0 +1,15 @@
+﻿using LiftNet.AzureBlob.Services;
+using LiftNet.Contract.Interfaces.Service;
+
+namespace LiftNet.Api.Extensions
+{
+    public static class ExtenalServiceExtension
+    {
+        public static IServiceCollection Register(this IServiceCollection services)
+        {
+            services.AddSingleton<IBlobService, BlobService>();
+
+            return services;
+        }
+    }
+}
