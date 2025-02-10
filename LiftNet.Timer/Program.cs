8f7aaf280f26cddf8079bd42ed81efbc6e0eba30
Dhuy2708 <vodinhhuy2708@gmail.com>
2025-02-11 01:54:10 +0700

    fix: repair logger

---
+++ LiftNet.Timer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LiftNet.Timer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
