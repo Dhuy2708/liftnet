363dd9df4f2de4219a2e8871177e22e0dc9e5f69
Dhuy2708 <vodinhhuy2708@gmail.com>
2025-05-16 06:11:30 +0700

    feat: intergrate chat services

---
+++ LiftNet.Contract/Interfaces/IServices/Indexes/IChatIndexService.cs
@@ -0,0 +1,17 @@
+﻿using LiftNet.Domain.Enums.Indexes;
+using LiftNet.Domain.Indexes;
+using LiftNet.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftNet.Contract.Interfaces.IServices.Indexes
+{
+    public interface IChatIndexService : IIndexBaseService<ChatMessageIndexData>, IDependency
+    {
+        Task<(List<ChatMessageIndexData> datas, string? nextPageToken)> GetMessages(string conversationId, int pageSize = 15, string? nextPageToken = null);
+        Task<int> SaveMessages(string conversationId, string userId, string message, ChatMessageType type = ChatMessageType.Text); 
+    }
+}
---
+++ LiftNet.Domain/Indexes/ChatMessageIndexData.cs
@@ -0,0 +1,31 @@
+﻿using LiftNet.Domain.Enums.Indexes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftNet.Domain.Indexes
+{
+    public class ChatMessageIndexData : IndexData
+    {
+        [JsonProperty(PropertyName = "conversationid")]
+        public string ConversationId
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message
+        {
+            get; set;
+        }
+
+        [JsonProperty(PropertyName = "type")]
+        public ChatMessageType Type
+        {
+            get; set;
+        }
+    }
+}
