73e4efbd4a98cfce91ee26312e48b52fedbf4186
Dhuy2708 <vodinhhuy2708@gmail.com>
2025-05-21 08:46:24 +0700

    feat: profile apis

---
+++ LiftNet.Api/Controllers/ProfileController.cs
@@ -0,0 +1,77 @@
+﻿using LiftNet.Api.Requests.Profiles;
+using LiftNet.Domain.Constants;
+using LiftNet.Domain.Response;
+using LiftNet.Handler.Profiles.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace LiftNet.Api.Controllers
+{
+    public class ProfileController : LiftNetControllerBase
+    {
+        public ProfileController(IMediator mediator, IServiceProvider serviceProvider) : base(mediator, serviceProvider)
+        {
+        }
+
+        [HttpPost("upload/avatar")]
+        [Authorize]
+        [ProducesResponseType(typeof(LiftNetRes), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UploadAvatar([FromForm] IFormFile file)
+        {
+            var req = new UploadAvatarCommand()
+            {
+                UserId = UserId,
+                File = file,
+            };
+            var result = await _mediator.Send(req);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return StatusCode(500, result);
+        }
+
+        [HttpPost("upload/cover")]
+        [Authorize]
+        [ProducesResponseType(typeof(LiftNetRes), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UploadCover([FromForm] IFormFile file)
+        {
+            var req = new UploadCoverCommand()
+            {
+                UserId = UserId,
+                File = file,
+            };
+            var result = await _mediator.Send(req);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return StatusCode(500, result);
+        }
+
+        [HttpPost("update")]
+        [Authorize]
+        [ProducesResponseType(typeof(LiftNetRes), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateProfile([FromBody] UpdateProfileRequest req)
+        {
+            var command = new UpdateProfileCommand()
+            {
+                UserId = UserId,
+                Age = req.Age,
+                Bio = req.Bio,
+                FirstName = req.FirstName,
+                LastName = req.LastName,
+                Gender = req.Gender,
+                Location = req.Location,
+            };
+            var result = await _mediator.Send(command);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return StatusCode(500, result);
+        }
+    }
+}
---
+++ LiftNet.Handler/Profiles/Commands/UploadCoverCommand.cs
@@ -0,0 +1,23 @@
+﻿using LiftNet.Domain.Response;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftNet.Handler.Profiles.Commands
+{
+    public class UploadCoverCommand : IRequest<LiftNetRes>
+    {
+        public string UserId
+        {
+            get; set;
+        }
+        public IFormFile File
+        {
+            get; set;
+        }
+    }
+}
