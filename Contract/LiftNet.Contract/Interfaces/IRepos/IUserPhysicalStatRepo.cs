c6b34f242f1f63b40ce592ec87e8ce7c1229090b
Dhuy2708 <vodinhhuy2708@gmail.com>
2025-06-07 20:20:05 +0700

    refactor: update project structure

--- Contract/LiftNet.Contract/Interfaces/IRepos/IUserPhysicalStatRepo.cs
+++
@@ -1,9 +0,0 @@
-using LiftNet.Domain.Entities;
-using LiftNet.Domain.Interfaces;
-
-namespace LiftNet.Contract.Interfaces.IRepos
-{
-    public interface IUserPhysicalStatRepo : IBaseRepo<UserPhysicalStat>
-    {
-    }
-} 
