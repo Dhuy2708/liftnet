9c261f8883f6e0e2fadb5c11ff8016a9354e2165
Dhuy2708 <vodinhhuy2708@gmail.com>
2025-06-07 20:42:02 +0700

    feat: list noti api

---
+++ Api/LiftNet.Api/Controllers/NotificationController.cs
@@ -0,0 +1,37 @@
+﻿using LiftNet.Contract.Views.Notis;
+using LiftNet.Domain.Response;
+using LiftNet.Handler.Appointments.Queries.Requests;
+using LiftNet.Handler.Notis.Queries.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace LiftNet.Api.Controllers
+{
+    public class NotificationController : LiftNetControllerBase
+    {
+        public NotificationController(IMediator mediator, IServiceProvider serviceProvider) : base(mediator, serviceProvider)
+        {
+        }
+
+        [HttpGet("list")]
+        [Authorize]
+        [ProducesResponseType(typeof(PaginatedLiftNetRes<NotificationView>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetNotifications([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            var req = new ListNotificationsQuery()
+            {
+                CallerId = UserId,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+            var result = await _mediator.Send(req);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return StatusCode(500, result);
+        }
+    }
+}
---
+++ Application/LiftNet.Handler/Notis/Queries/Requests/ListNotificationsQuery.cs
@@ -0,0 +1,29 @@
+﻿using LiftNet.Contract.Views.Notis;
+using LiftNet.Domain.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftNet.Handler.Notis.Queries.Requests
+{
+    public class ListNotificationsQuery : IRequest<PaginatedLiftNetRes<NotificationView>>
+    {
+        public string CallerId
+        {
+            get; set;
+        }
+
+        public int PageNumber
+        {
+            get; set;
+        } = 1;
+
+        public int PageSize
+        {
+            get; set;
+        } = 10;
+    }
+}
