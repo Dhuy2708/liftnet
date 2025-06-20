e57180b867af704796f0524730d51cabab725b16
Dhuy2708 <vodinhhuy2708@gmail.com>
2025-06-20 18:07:03 +0700

    feat: comment feed api

---
+++ Api/LiftNet.Api/Requests/Feeds/CommentFeedRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace LiftNet.Api.Requests.Feeds
+{
+    public class CommentFeedRequest
+    {
+        public string FeedId
+        {
+            get; set;
+        }
+        public string Comment
+        {
+            get; set;
+        }
+        public string? ParentId
+        {
+            get; set;
+        }
+    }
+}
